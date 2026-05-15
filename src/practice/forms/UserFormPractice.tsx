bfc86ff2126ee5bf29e29314c4567fa2f24ed74f
DanielRodJ <106783688+DanielRodJ@users.noreply.github.com>
2026-05-15 16:17:44 -0600

    refactor: remove unused files and obsolete modules

--- src/practice/forms/UserFormPractice.tsx
+++
@@ -1,9 +0,0 @@
-type Product = {
-  id: number;
-  nombreProducto: string;
-  precio?: number;
-}
-
-function TsPractice(productos: Product[]) : Product[] {
-  return productos.filter(p => p.precio !== undefined);
-}
